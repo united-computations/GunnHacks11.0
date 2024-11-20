155544edef8fc9679e91a1525f6a22a5f1ec9bf5
Ian Cheung <ian@iancheung.dev>
2024-11-20 10:03:54 -0800

    Create page.tsx

---
+++ src/app/conduct/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+    title: 'Code of Conduct | GunnHaXI'
+}
+
+export default function Register() {
+    // redirect("https://docs.google.com/forms/d/e/1FAIpQLScVPB66QN6VdTLCzdj91xYIkH0RGDLeJ_gAU7V6TYRIrG1dRw/viewform");
+    return (
+        <div className="flex justify-center">
+                Coming soon...
+        </div>
+    )
+}
