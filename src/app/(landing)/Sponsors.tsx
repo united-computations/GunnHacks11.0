20f54fe50530433601f21a23a10270d80eb859b9
Ian Cheung <ian@iancheung.dev>
2024-11-20 10:49:22 -0800

    Update Sponsors.tsx

--- src/app/(landing)/Sponsors.tsx
+++ src/app/(landing)/Sponsors.tsx
@@ -10,7 +10,7 @@ export default function Sponsors() {
                 Find our sponsorship prospectus <a target="_blank" rel="noopener noreferrer" href="/docs/sponsorship_prospectus.pdf">here</a>.
             </p>
 
-            <div className="pt-8 pb-2 flex flex-wrap gap-x-5 gap-y-2 items-center justify-center">
+            {/*<div className="pt-8 pb-2 flex flex-wrap gap-x-5 gap-y-2 items-center justify-center">
                 <Sponsor href="https://www.axure.com/" src="/sponsors/axure.svg" alt="Axure" />
                 <Sponsor href="https://www.applied-computing.org/" src="/sponsors/ACF.png" alt="Applied Computing Foundation" />
                 <Sponsor href="https://gen.xyz/" src="/sponsors/XYZ.svg" alt="XYZ" />
@@ -21,14 +21,14 @@ export default function Sponsors() {
                 <Sponsor href="https://www.mitchellparkteenservices.org/thinkfund" src="/sponsors/thinkfund.webp" alt="Think Fund" width="md" />
                 <Sponsor href="https://www.cityofpaloalto.org/" src="/sponsors/city-of-palo-alto.png" alt="City of Palo Alto" />
 
-                {/* <Sponsor href="https://www.wolfram.com" src="/sponsors/wolfram.svg" alt="Wolfram" />
+                <Sponsor href="https://www.wolfram.com" src="/sponsors/wolfram.svg" alt="Wolfram" />
                 <Sponsor href="https://artofproblemsolving.com" src="/sponsors/aops.png" alt="AoPS" />
                 <Sponsor href="https://mule.to/p3df" src="/sponsors/sticker-mule.png" alt="Sticker Mule" />
                 
                 <Sponsor href="https://linktr.ee/leadinglearners" src="/sponsors/leading-learners.png" alt="Leading Learners" />
                 <Sponsor href="https://www.leangap.org/" src="/sponsors/leangap.svg" alt="Leangap" />
-                <Sponsor href="https://gunn.paloaltopta.org/" src="/sponsors/ptsa.png" alt="Gunn PTSA" /> */}
-            </div>
+                <Sponsor href="https://gunn.paloaltopta.org/" src="/sponsors/ptsa.png" alt="Gunn PTSA" />
+            </div> */}
 
             {/* <h1 className='text-3xl font-bold border-b-2 border-current mb-4'>Partners</h1>
             <div className="pt-8 pb-2 flex flex-wrap gap-x-5 gap-y-2 items-center justify-center">
