30eb5ee1a5bbf81e5496eda00fd7f71cef9b7705
Ian Cheung <ian@iancheung.dev>
2024-11-20 10:17:11 -0800

    Update page.tsx

--- src/app/conduct/page.tsx
+++ src/app/conduct/page.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
 export default function Register() {
     // redirect("https://docs.google.com/forms/d/e/1FAIpQLScVPB66QN6VdTLCzdj91xYIkH0RGDLeJ_gAU7V6TYRIrG1dRw/viewform");
     return (
-        <div className="flex justify-center" style="margin-top: 50px; color: white; font-size: 50px">
-                Coming soon...
+        <div className="flex justify-center" style={{ marginTop: '50px', color: 'white', fontSize: '50px' }}>
+            Coming soon...
         </div>
     )
 }
