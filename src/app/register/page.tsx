2dd526ab39565ec108fe9d77d97b0aed3be5cd9b
Ian Cheung <ian@iancheung.dev>
2024-11-20 09:57:32 -0800

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-    title: 'Register | GunnHaX'
+    title: 'Register | GunnHaXI'
 }
 
 export default function Register() {
-    redirect("https://docs.google.com/forms/d/e/1FAIpQLSdF_KFH0pH_ryAx3uYglID2ITeXvoTaHfTh2i9YMgnlwLC91A/viewform");
+    redirect("https://docs.google.com/forms/d/e/1FAIpQLScVPB66QN6VdTLCzdj91xYIkH0RGDLeJ_gAU7V6TYRIrG1dRw/viewform");
     // return (
     //     <div className="flex justify-center">
     //         <iframe
