ba64c1a4d906749ea606917a7346f587499f64a4
Ian Cheung <ian@iancheung.dev>
2024-11-20 17:35:07 -0800

    Update page.tsx

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -9,23 +9,24 @@ import Sponsors from './Sponsors';
 import Footer from '../../components/Footer';
 import Head from 'next/head';
 
-
 export default function Home() {
     return (
-        <Head>
-            <title>GunnHacks 11.0</title>
-            <meta name="description" content="Make, Build, Create & Learn. It’s GunnHacks 11.0, Gunn’s 24‑hour high school hackathon." />
-        </Head>
-        <div>
-            {/* <Header /> */}
-            <Heading />
-            <About />
-            <FAQ />
-            <Schedule />
-            <Prizes />
-            {/* <Judges /> */}
-            <Sponsors />
-            <Footer />
-        </div>
-    )
+        <>
+            <Head>
+                <title>GunnHacks 11.0</title>
+                <meta name="description" content="Make, Build, Create & Learn. It’s GunnHacks 11.0, Gunn’s 24‑hour high school hackathon." />
+            </Head>
+            <div>
+                {/* <Header /> */}
+                <Heading />
+                <About />
+                <FAQ />
+                <Schedule />
+                <Prizes />
+                {/* <Judges /> */}
+                <Sponsors />
+                <Footer />
+            </div>
+        </>
+    );
 }
