c6b6b54e6027facb1424cddbe595799edcfd394c
Ian Cheung <ian@iancheung.dev>
2024-11-20 17:20:24 -0800

    Update head.tsx

--- src/app/(landing)/head.tsx
+++ src/app/(landing)/head.tsx
@@ -1,8 +1,8 @@
 export default function Head() {
     return (
         <>
-            <title>GunnHacks 10.0</title>
-            <meta name="description" content="Make, Build, Create & Learn. It’s GunnHacks 10.0, Gunn’s 24‑hour high school hackathon." />
+            <title>GunnHacks 11.0</title>
+            <meta name="description" content="Make, Build, Create & Learn. It’s GunnHacks 11.0, Gunn’s 24‑hour high school hackathon." />
         </>
     )
 }
