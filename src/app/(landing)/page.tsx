671f335dc20a4a662a7fedd92977dbf1d4295778
Ian Cheung <ian@iancheung.dev>
2024-11-20 17:33:09 -0800

    Update page.tsx

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -7,10 +7,15 @@ import Judges from './Judges';
 import FAQ from './FAQ';
 import Sponsors from './Sponsors';
 import Footer from '../../components/Footer';
+import Head from 'next/head';
 
 
 export default function Home() {
     return (
+        <Head>
+            <title>GunnHacks 11.0</title>
+            <meta name="description" content="Make, Build, Create & Learn. It’s GunnHacks 11.0, Gunn’s 24‑hour high school hackathon." />
+        </Head>
         <div>
             {/* <Header /> */}
             <Heading />
@@ -18,7 +23,7 @@ export default function Home() {
             <FAQ />
             <Schedule />
             <Prizes />
-            <Judges />
+            {/* <Judges /> */}
             <Sponsors />
             <Footer />
         </div>
