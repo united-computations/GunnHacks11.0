d3924fc038ebbd5419f70b0f5227d1bfc6afb584
Ian Cheung <ian@iancheung.dev>
2024-12-10 20:25:18 -0800

    Update Heading.tsx

--- src/app/(landing)/Heading.tsx
+++ src/app/(landing)/Heading.tsx
@@ -43,7 +43,7 @@ export default function Heading() {
     return (
         <section id="heading" className="text-white text-center h-screen relative flex flex-col items-center justify-center p-5 pb-[20vh]">
             <div className={`mb-2 relative flex flex-col items-center md:block ${overpass.className}`}>
-                <span className="pt-2 text-6xl md:text-9xl tracking-wider"><img className="mb-1" src="/gunnhacks11-dark.png" alt="GunnHacks 11.0 Logo" draggable="false" /></span>
+                <span className="pt-2 text-6xl md:text-9xl tracking-wider"><img className="mb-2" src="/gunnhacks11-dark.png" alt="GunnHacks 11.0 Logo" draggable="false" /></span>
                 {/*<span className="pt-2 text-6xl md:text-9xl tracking-wider">GunnHa<span className="invisible">XI</span></span>
                 <img className="absolute w-32 md:w-64 left-[15rem] top-[-1.6rem] md:left-[calc(26rem)] md:top-[-2.9rem]" src="/X.svg" alt="X Logo" />*/}
                 <div className="ml-3 md:mt-[-.5rem] flex flex-col items-center md:block">
