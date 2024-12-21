ca11d5eb048b37da6b17a3d4a8390db2009f7611
Daniel Kou <daniel.j.kou@gmail.com>
2024-12-20 18:18:04 -0800

    Update Sponsors.tsx

--- src/app/(landing)/Sponsors.tsx
+++ src/app/(landing)/Sponsors.tsx
@@ -7,7 +7,6 @@ export default function Sponsors() {
         <Section title="Sponsors" id="sponsors">
             <p>
                 To sponsor our event, email us at <a href="mailto:ghshacks@gmail.com">ghshacks@gmail.com</a>.
-                /* Find our sponsorship prospectus <a target="_blank" rel="noopener noreferrer" href="/docs/sponsorship_prospectus.pdf">here</a>. */
             </p>
 
             <div className="pt-8 pb-2 flex flex-wrap gap-x-5 gap-y-2 items-center justify-center">
