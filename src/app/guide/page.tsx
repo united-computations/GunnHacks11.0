ab762acd7680e16b2f83f0764d07fda44c4364ac
Daniel Kou <daniel.j.kou@gmail.com>
2025-01-25 20:57:41 -0800

    Update page.tsx

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-    title: 'Hacker Guide | GunnHaX'
+    title: 'Hacker Guide | GunnHacks'
 }
 
 export default function Guide() {
