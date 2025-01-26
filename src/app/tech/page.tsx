54a52723bb2532e2c1aa046ccf73110218902134
Daniel Kou <daniel.j.kou@gmail.com>
2025-01-25 20:58:14 -0800

    Update page.tsx

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-    title: 'Team Selection Form | GunnHaX'
+    title: 'Team Selection Form | GunnHacks'
 }
 
 export default function Guide() {
