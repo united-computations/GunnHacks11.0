1c4704509bb8d78944f1ebe9801beb5f68ad2b49
Daniel Kou <daniel.j.kou@gmail.com>
2025-01-25 20:57:31 -0800

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-    title: 'Register | GunnHaXI'
+    title: 'Register | GunnHacks'
 }
 
 export default function Register() {
