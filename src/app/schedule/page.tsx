b194c4b58835b8577129183afff2464eb69d6f61
Daniel Kou <daniel.j.kou@gmail.com>
2025-01-25 21:05:02 -0800

    Update page.tsx

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -6,5 +6,5 @@ export const metadata: Metadata = {
 }
 
 export default function Schedule() {
-    redirect("https://docs.google.com/document/d/1eJ0y7DYtzaWN9ICCv4c-Mf0VonRpU17meFKQ6TtV50c/edit?tab=t.0"");
+    redirect("https://docs.google.com/document/d/1eJ0y7DYtzaWN9ICCv4c-Mf0VonRpU17meFKQ6TtV50c/edit?tab=t.0");
 }
